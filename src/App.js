9db8679db5d90e959d7ac26f308535c848c2ec17
roasx <132033098+roasx@users.noreply.github.com>
2023-05-23 17:32:25 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import "./styles.css";
+import React, { useState } from "react";
+import cabbage from "./assets/image1.jpeg";
+import mango from "./assets/image2.jpeg";
+import fig from "./assets/image3.jpeg";
+import gaze from "./assets/image4.jpeg";
+import peach from "./assets/image5.jpeg";
+import avocado from "./assets/image6.jpeg";
+
+const images = [cabbage, mango, fig, gaze, peach, avocado];
+
+const Loading = ({ calculatedWidth }) => (
+  <aside>
+    <div class="loading-bar">
+      <label htmlFor="images-loaded">Loading all your favorite images...</label>
+      <progress id="images-loaded" max="100" value={calculatedWidth}></progress>
+    </div>
+  </aside>
+);
+
+const App = () => {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [numLoaded, setNumLoaded] = useState(0);
+
+  const handleClick = () => {
+    const length = images.length - 1;
+
+    setCurrentImage((currentImage) => {
+      return currentImage < length ? currentImage + 1 : 0;
+    });
+  };
+
+  const handleImageLoad = () => {
+    setNumLoaded((numLoaded) => numLoaded + 1);
+  };
+
+  return (
+    <section>
+      <header>
+        <h1>Zesty</h1>
+        <h2>
+          A photography project
+          <br />
+          by Ella Fieldling
+        </h2>
+      </header>
+
+      <figure>
+        {numLoaded < images.length && (
+          <Loading calculatedWidth={(numLoaded / images.length) * 100} />
+        )}
+        <figcaption>
+          {currentImage + 1} / {images.length}
+        </figcaption>
+        {images.map((imageURL, index) => (
+          <img
+            src={imageURL}
+            onClick={handleClick}
+            onLoad={handleImageLoad}
+            key={imageURL}
+            alt=""
+            // style={{opacity: currentImage === index ? 1 : 0}}
+            className={currentImage === index ? "display" : "hide"}
+          />
+        ))}
+      </figure>
+    </section>
+  );
+};
+
+export default App;
